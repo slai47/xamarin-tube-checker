23fd5f69af639848b71cbd8c3902061fc4a4171e
Harrison Frede <harrisonfrede@gmail.com>
2019-03-27 12:07:26 -0400

    Adding in api files.

---
+++ NotifyYou/NotifyYou/API/YoutubeApi.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using NotifyYou.Models;
+
+namespace NotifyYou.API
+{
+    public class YoutubeApi : IYoutube
+    {
+        public YoutubeApi()
+        {
+        }
+
+        public async IEnumerable<Channel> GetChannels(string search = null)
+        {
+            List<Channel> channels = new List<Channel>();
+
+
+
+
+            return channels;
+        }
+    }
+
+    public interface IYoutube
+    {
+        Task<IEnumerable<Channel>> GetChannels(string search = null);
+    }
+}
